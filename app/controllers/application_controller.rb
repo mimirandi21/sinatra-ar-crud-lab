936ee1d7cca1cc344a776129ea9df03af2773f60
Randi Schreiner <mimirandi21@gmail.com>
2020-11-12 17:59:08 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,53 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  post '/articles' do
+    Article.create(params)
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  
+
+  get '/articles/:id/edit' do
+    @articles = Article.find_by_id(params[:id])
+    erb :edit  
+  end
+
+  get '/articles/:id' do
+    @articles = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  
+
+  patch '/articles/:id' do
+    @articles = Article.find_by_id(params[:id])
+    
+    @articles = @articles.update(params[:article])
+    
+    redirect "/articles/#{@articles.id}"
+  end
+
+  delete '/articles/:id/delete' do
+    
+    Article.delete(params[:id])
+    erb :delete
+  end
+
+  
+
+  
+
 end
